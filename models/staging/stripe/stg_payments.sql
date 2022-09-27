c77f8b2638f969372e87355bf09e4038fd3d5c1e
Anna Maria Tango <annamariatango@gmail.com>
2022-09-27 14:02:18 +0000

    updated stg files

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -7,5 +7,4 @@ select
     -- amount is stored in cents, convert it to dollars
     amount / 100 as amount,
     created as created_at
-
-from dbt-bigquery-363116.stripe.payment
+from {{ source('stripe', 'payment') }}
